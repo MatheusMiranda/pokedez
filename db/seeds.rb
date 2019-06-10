f8c55d118fcc7118dbc274eae694bc00af5ddb5b
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-06-10 20:41:29 -0300

    Add seed to populate database
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,17 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'net/http'
+require 'json'
+
+pokeapi_base_url = 'https://pokeapi.co/api/v2/'
+
+for pokemon_id in 1..151 do
+  sleep(1)
+  request_result = Net::HTTP.get(URI.parse(pokeapi_base_url + 'pokemon/' + pokemon_id.to_s))
+
+  pokemon_data = JSON.parse(request_result)
+
+  Pokemon.create(name: pokemon_data['name'])
+end
