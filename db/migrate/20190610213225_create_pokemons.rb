c2201584c05a82174c436d5c663aea1cb0e42e99
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-06-14 15:30:13 -0300

    Add evolution attribute to Pokemon
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- db/migrate/20190610213225_create_pokemons.rb
+++ db/migrate/20190610213225_create_pokemons.rb
@@ -3,6 +3,7 @@ class CreatePokemons < ActiveRecord::Migration[5.2]
     create_table :pokemons do |t|
       t.string :name
       t.string :sprite
+      t.integer :evolution
 
       t.timestamps
     end
