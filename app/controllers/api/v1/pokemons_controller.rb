1895a1baf5211c63e9226876235620947f5e1842
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-06-14 11:54:08 -0300

    Add methods to create, update and delete pokemons
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- app/controllers/api/v1/pokemons_controller.rb
+++ app/controllers/api/v1/pokemons_controller.rb
@@ -1,4 +1,5 @@
 class Api::V1::PokemonsController < ApplicationController
+  before_action :set_pokemon, only: [:show, :edit, :update, :destroy]
 
   # GET /api/v1/pokemons
   def index
@@ -7,7 +8,60 @@ class Api::V1::PokemonsController < ApplicationController
         id: pokemon.id, name: pokemon.name
       }
     }
+
     render json: pokemons
   end
 
+  #GET /api/v1/pokemons/:pokemon_id
+  def show
+    pokemon = Pokemon.find(params[:id])
+
+    render json: pokemon
+  end
+
+  # POST /api/v1/pokemons
+  def create
+    @pokemon = Pokemon.new(pokemon_params)
+
+    respond_to do |format|
+      if @pokemon.save
+        format.html { redirect_to @pokemon, notice: 'Pokemon was successfully created.' }
+        format.json { render :show, status: :created, location: @pokemon }
+      else
+        format.html { render :new }
+        format.json { render json: @pokemon.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PATCH/PUT /pokemons/1
+  def update
+    respond_to do |format|
+      if @pokemon.update(pokemon_params)
+        format.html { redirect_to @pokemon, notice: 'Pokemon was successfully updated.' }
+        format.json { render :show, status: :ok, location: @pokemon }
+      else
+        format.html { render :edit }
+        format.json { render json: @pokemon.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /pokemons/1
+  def destroy
+    @pokemon.destroy
+    respond_to do |format|
+      format.html { redirect_to pokemons_url, notice: 'Pokemon was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def set_pokemon
+      @pokemon = Pokemon.find(params[:id])
+    end
+
+    def pokemon_params
+      params.require(:pokemon).permit(:name, :teste)
+    end
 end
