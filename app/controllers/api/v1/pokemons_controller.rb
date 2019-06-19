ee01f8684cd7b1b75db888eacadd30d0a6dd3247
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-06-19 10:48:39 -0300

    Update index action to accept filter param
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- app/controllers/api/v1/pokemons_controller.rb
+++ app/controllers/api/v1/pokemons_controller.rb
@@ -1,11 +1,16 @@
 class Api::V1::PokemonsController < ApplicationController
   before_action :set_pokemon, only: [:show, :edit, :update, :destroy]
+  include ActionController::MimeResponds
 
   # GET /api/v1/pokemons
   def index
-    pokemons = Pokemon.all.map{ |pokemon|
+    filter_param = params[:filter] || ""
+
+    pokemons = Pokemon.all.select{
+      |pokemon| pokemon.name.downcase.include? filter_param.downcase
+    }.map{ |pokemon|
       {
-        id: pokemon.id, name: pokemon.name
+        id: pokemon.id, name: pokemon.name, types: pokemon.types
       }
     }
 
@@ -14,7 +19,7 @@ class Api::V1::PokemonsController < ApplicationController
 
   #GET /api/v1/pokemons/:pokemon_id
   def show
-    render json: @pokemon
+    render json: { id: @pokemon.id, name: @pokemon.name, types: @pokemon.types, evolutions: get_evolutions}
   end
 
   # POST /api/v1/pokemons
@@ -55,11 +60,39 @@ class Api::V1::PokemonsController < ApplicationController
   end
 
   private
+    def get_evolutions
+      evolutions = []
+
+      first_evolutions = []
+      second_evolutions = []
+
+      @pokemon.evolutions.each do |evolution|
+        evolution_pokemon = Pokemon.find_by("name": evolution)
+        if evolution_pokemon != nil
+          first_evolutions << evolution_pokemon
+        end
+      end
+
+      first_evolutions.each do |evol|
+        evol.evolutions.each do |evolution|
+          evolution_pokemon = Pokemon.find_by("name": evolution)
+          if evolution_pokemon!= nil
+            second_evolutions << evolution_pokemon
+          end
+        end
+      end
+
+      evolutions << first_evolutions
+      evolutions << second_evolutions
+
+      evolutions
+    end
+
     def set_pokemon
       @pokemon = Pokemon.find(params[:id])
     end
 
     def pokemon_params
-      params.require(:pokemon).permit(:name, :types, :evolutions, :sprite)
+      params.require(:pokemon).permit(:name, :sprite, types: [], evolutions: [])
     end
 end
