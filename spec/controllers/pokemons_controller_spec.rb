a975b60f80fd0588acbe5e4003432de097a00e94
Matheus Miranda <matheusmirandalacerda@gmail.com>
2019-06-26 23:06:49 -0300

    Add tests for actions 'index' and 'show'
    Signed-off-by: Matheus Miranda <matheusmirandalacerda@gmail.com>

--- spec/controllers/pokemons_controller_spec.rb
+++ spec/controllers/pokemons_controller_spec.rb
@@ -26,12 +26,7 @@ describe Api::V1::PokemonsController do
   end
 
   describe "GET #show - get pokemon by id", type: :request do
-    let!(:pokemon) { create(:pokemon, name: "jorel", types: ["bug", "poison"],
-                            photo_file_name: "jorel.jpg",
-                            photo_content_type: "image/jpeg",
-                            photo_file_size: 2588,
-                            photo_updated_at: Time.zone.now
-                            ) }
+    let!(:pokemon) { create(:pokemon, name: "jorel", types: ["bug", "poison"]) }
 
     before do
       get "/api/v1/pokemons/#{pokemon.id}"
@@ -59,39 +54,17 @@ describe Api::V1::PokemonsController do
     end
   end
 
+  describe "POST #create - create pokemon", type: :request do
+    let(:pokemon) { create(:pokemon) }
 
+    before do
+      post '/api/v1/pokemons', params: { pokemon: pokemon.attributes
+                                       }
+    end
 
-  #describe 'POST create' do
-  #  before do
-  #    @process = create(:legislation_process, debate_start_date: Date.current - 3.days, debate_end_date: Date.current + 2.days)
-  #    @question = create(:legislation_question, process: @process, title: "Question 1")
-  #    @user = create(:user, :level_two)
-  #    @unverified_user = create(:user)
-  #  end
+    it 'returns a created status' do
+      expect(response).to have_http_status(:created)
+    end
+  end
 
-  #  it 'creates an comment if the comments are open' do
-  #    sign_in @user
-
-  #    expect do
-  #      xhr :post, :create, comment: {commentable_id: @question.id, commentable_type: "Legislation::Question", body: "a comment"}
-  #    end.to change { @question.reload.comments_count }.by(1)
-  #  end
-
-  #  it 'does not create a comment if the comments are closed' do
-  #    sign_in @user
-  #    @process.update_attribute(:debate_end_date, Date.current - 1.day)
-
-  #    expect do
-  #      xhr :post, :create, comment: {commentable_id: @question.id, commentable_type: "Legislation::Question", body: "a comment"}
-  #    end.not_to change { @question.reload.comments_count }
-  #  end
-
-  #  it 'does not create a comment for unverified users when the commentable requires it' do
-  #    sign_in @unverified_user
-
-  #    expect do
-  #      xhr :post, :create, comment: {commentable_id: @question.id, commentable_type: "Legislation::Question", body: "a comment"}
-  #    end.not_to change { @question.reload.comments_count }
-  #  end
-  #end
 end
